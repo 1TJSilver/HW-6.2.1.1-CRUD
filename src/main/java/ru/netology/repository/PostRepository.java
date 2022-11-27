1263585550042f5d582b14e4bf3c77ca08fad05d
John Terve <89539449797@mail.ru>
2022-11-27 07:53:58 +0300

    Add comcurrent HashMap

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,46 +1,43 @@
 package ru.netology.repository;
-
 import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
-
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
-// Stub
 public class PostRepository {
-  static AtomicInteger counter = new AtomicInteger(1);
-  final static int NEW_POST = 0;
-  List<Post> repository;
-  public PostRepository(){
-    this.repository = new CopyOnWriteArrayList<>();
-  }
-  public List<Post> all() {
-    return repository;
-  }
+    static AtomicInteger counter = new AtomicInteger(1);
+    final static int NEW_POST = 0;
+    ConcurrentHashMap<Long, Post> repository;
 
-  public Optional<Post> getById(long id) {
-    return repository.stream()
-            .filter(x -> x.getId() == id)
-            .findFirst();
-  }
-
-  public Post save(Post post) {
-    if (post.getId() == NEW_POST){
-      post.setId(counter.getAndIncrement());
-      repository.add(post);
-    } else {
-      removeById(post.getId());
-      repository.add(post);
+    public PostRepository() {
+        this.repository = new ConcurrentHashMap<>();
     }
-    return post;
-  }
 
-  public void removeById(long id) {
-    Optional<Post> optional = getById(id);
-    repository.remove(optional.orElseThrow(NotFoundException::new));
-  }
+    public List<Post> all() {
+        return repository.values().stream().toList();
+    }
+
+    public Optional<Post> getById(long id) {
+        return repository.values().stream()
+                .filter(x -> x.getId() == id)
+                .findFirst();
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == NEW_POST) {
+            post.setId(counter.getAndIncrement());
+            repository.put(post.getId(), post);
+        } else {
+            removeById(post.getId());
+            repository.put(post.getId(), post);
+        }
+        return post;
+    }
+
+    public void removeById(long id) {
+        Optional<Post> optional = getById(id);
+        repository.remove(optional.orElseThrow(NotFoundException::new));
+    }
 }
